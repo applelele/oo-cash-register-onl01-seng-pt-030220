b5f25238b88336a63a64a7a18c09e382591793fd
YUKIKO IKEDA <applelele@gmail.com>
2020-03-29 22:54:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,15 @@
 class CashRegister
-  attr_accessor :total, :discount, :title
+  attr_accessor :total, :employee_discount, :title
 
-  def initialize(discount = nil)
+  def initialize(employee_discount = nil)
     @total = 0.00
-    @discount = discount
+    @employee_discount = employee_discount
     @title = []
     # @@cart << self
   end
 
   def discount
-    self.discount
+    self.employee_discount
   end
 
   def add_item(title, price, quantity = 1)
@@ -33,8 +33,8 @@ class CashRegister
   end
 
   def apply_discount
-    if @discount
-      @total = @total * (1 - (@discount.to_f / 100.0))
+    if @employee_discount
+      @total = @total * (1 - (@employee_discount.to_f / 100.0))
       "After the discount, the total comes to $#{@total.to_i}."
     else
       "There is no discount to apply."
